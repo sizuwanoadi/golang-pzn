03aae32e28b99f3ceb5c10a271138105c793d4a4
Adi Siswanto <adi15siswanto@gmail.com>
2023-02-07 09:35:48 +0700

    update learning

---
+++ bag_1_golang_dasar/4-tipe-data-string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Sizu")
+	fmt.Println("Adi Siswanto")
+	fmt.Println("Adi Siswanto Bilek")
+	fmt.Println("")
+	fmt.Println(len("Rayhan Wangsaff"))
+	fmt.Println("Rayhan Wangsaff"[0])
+	fmt.Println("Rayhan Wangsaff"[1])
+}
