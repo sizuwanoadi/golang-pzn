b1830dc0d6e47c7e000b3fd1caae59177178a976
Adi Siswanto <adi15siswanto@gmail.com>
2023-02-13 17:11:57 +0700

    update goroutines

---
+++ golang-goroutines/goroutines_test.go
@@ -0,0 +1,46 @@
+package golang_goroutines
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func RunHelloWorld() {
+	fmt.Println("Hello World")
+}
+
+func TestCreateGoroutine(t *testing.T) {
+	go RunHelloWorld()
+	fmt.Println("UPS")
+
+	time.Sleep(1 * time.Second)
+}
+
+func DisplayNumber(number int) {
+	fmt.Println("Display", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(5 * time.Second)
+}
+
+func TestCreateChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go func() {
+		time.Sleep(2 * time.Second)
+		channel <- "Adi Siswanto"
+		fmt.Println("Selesai mengirim data ke channel")
+	}()
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(5 * time.Second)
+}
